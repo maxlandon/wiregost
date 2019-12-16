b10c99a5607ab484752b08075e9d68321ea60b42
Maxime Landon <maximelandon@gmail.com>
2019-12-16 03:52:36 +0100

    Part of refactoring started at commit c241ebd: refactor into a central Spectre object, and make a cmd executable file for launching server.

---
+++ cmd/server/spectre.go
@@ -0,0 +1,14 @@
+package main
+
+// This file is the main Spectre executable.
+
+import (
+	"github.com/maxlandon/wiregost/internal/server/core"
+)
+
+func main() {
+
+	// Instantiates a Spectre
+	core.NewSpectre()
+
+}
---
+++ internal/server/core/spectre.go
@@ -0,0 +1,48 @@
+package core
+
+// The core package is the backbone of the WireGost server, Spectre.
+
+// It will interface with various components such as authentication services,
+// listeners, databases, etc.
+
+type Spectre struct {
+	// Database
+	DBManager *DBManager
+	// Services
+	UserManager *UserManager
+	// RPC Servers
+	ClientRPC *ClientRPC
+}
+
+func NewSpectre() *Spectre {
+	spectre := &Spectre{
+		DBManager:   NewDBManager(),
+		UserManager: NewUserManager(),
+		ClientRPC:   NewClientRPC(),
+	}
+
+	// Make DBManager available to all services needing it.
+	spectre.RegisterDatabaseToServices()
+
+	// Register all services to the ClientRPC.
+	spectre.RegisterServicesToRPC()
+
+	// Start RPC
+	spectre.ClientRPC.Start()
+
+	return spectre
+}
+
+func (s *Spectre) RegisterDatabaseToServices() error {
+	// UserManager
+	s.UserManager.database = s.DBManager
+	s.UserManager.GetUsers()
+
+	return nil
+}
+
+func (s *Spectre) RegisterServicesToRPC() error {
+	RegisterUserManagerServer(s.ClientRPC.server, s.UserManager)
+
+	return nil
+}
