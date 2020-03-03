5e8e09a34a32c76ce061aea85bae1d9c3bf57df3
Maxime Landon <maximelandon@gmail.com>
2020-03-03 02:27:26 +0100

    Add filesystem comp

--- client/completers/agent-filesystem.go
+++ client/completers/agent-filesystem.go
@@ -27,7 +27,6 @@ import (
 	ghostpb "github.com/maxlandon/wiregost/protobuf/ghost"
 )
 
-// AutoCompleter is the autocompletion engine
 type implantPathCompleter struct {
 	Command *commands.Command
 }
@@ -58,13 +57,32 @@ func (pc *implantPathCompleter) Do(ctx *commands.ShellContext, line []rune, pos 
 		}
 	default:
 		if strings.HasSuffix(string(line), "/") {
-			// Trim the non needed slash
-			linePath = strings.TrimSuffix(string(line), "/")
+			// If the the line is just "/", it means we start from filesystem root
+			if string(line) == "/" {
+				linePath = "/"
+			} else if string(line) == "~/" {
+				// If we look for "~", we need to build the path manually
+				linePath = filepath.Join("/home", ctx.CurrentAgent.Username)
+
+			} else if strings.HasPrefix(string(line), "~/") && string(line) != "~/" {
+				// If we used the "~" at the beginning, we still need to build the path
+				homePath := filepath.Join("/home", ctx.CurrentAgent.Username)
+				linePath = filepath.Join(homePath, strings.TrimPrefix(string(line), "~/"))
+			} else {
+				// Trim the non needed slash
+				linePath = strings.TrimSuffix(string(line), "/")
+			}
+		} else if strings.HasPrefix(string(line), "~/") && string(line) != "~/" {
+			// If we used the "~" at the beginning, we still need to build the path
+			homePath := filepath.Join("/home", ctx.CurrentAgent.Username)
+			linePath = filepath.Join(homePath, filepath.Dir(strings.TrimPrefix(string(line), "~/")))
+			lastPath = filepath.Base(string(line))
 
 		} else if string(line) == "" {
 			linePath = "."
 		} else {
-			linePath = string(line)
+			// linePath = string(line)
+			linePath = filepath.Dir(string(line))
 			lastPath = filepath.Base(string(line))
 		}
 	}
