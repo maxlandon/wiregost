e7455a5d78586a47a08e7f38b86923191315b914
Maxime Landon <maximelandon@gmail.com>
2020-03-04 08:50:31 +0100

    Remove maltego directory

--- maltego/src/EffectiveCouscous/resources/images/device/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Maxime Landon'
-__copyright__ = 'Copyright 2019, MSM Project'
-__credits__ = []
-
-__license__ = 'GPLv3'
-__version__ = '0.2'
-__maintainer__ = 'Maxime Landon'
-__email__ = 'maximelandon@gmail.com'
-__status__ = 'Development'
