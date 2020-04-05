87db7b5fcfbeb8107b008d46a18a654c91962d2f
Maxime Landon <maximelandon@gmail.com>
2020-04-05 06:52:33 +0200

    Refactor/move shell usage code

--- client/util/shell-exec.go
+++ client/util/shell-exec.go
@@ -17,16 +17,26 @@
 package util
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/evilsocket/islazy/str"
 )
 
-// Shell - Execute shell command on Linux/MacOS
-func Shell(args []string) (string, error) {
-	cmd := strings.Join(args, " ")
-	return Exec("/bin/sh", []string{"-c", cmd})
+// Shell - Use the system shell transparently through the console
+func Shell(args []string) error {
+	out, err := Exec(args[0], args[1:])
+	if err != nil {
+		fmt.Printf(CommandError+"%s \n", err.Error())
+		return nil
+	}
+
+	// Print output
+	fmt.Println(out)
+
+	return nil
 }
 
 // Exec - Execute a program
@@ -36,9 +46,36 @@ func Exec(executable string, args []string) (string, error) {
 		return "", err
 	}
 
-	raw, err := exec.Command(path, args...).CombinedOutput()
+	cmd := exec.Command(path, args...)
+
+	// Load OS environment
+	cmd.Env = os.Environ()
+
+	out, err := cmd.CombinedOutput()
+
 	if err != nil {
 		return "", err
 	}
-	return str.Trim(string(raw)), nil
+	return str.Trim(string(out)), nil
+}
+
+// inputIsBinary - Check if first input is a system program
+func inputIsBinary(args []string) bool {
+	_, err := exec.LookPath(args[0])
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// LoadSystemEnv - Loads all system environment variables
+func LoadSystemEnv() error {
+	env := os.Environ()
+
+	for _, kv := range env {
+		key := strings.Split(kv, "=")[0]
+		value := strings.Split(kv, "=")[1]
+		SystemEnv[key] = value
+	}
+	return nil
 }
