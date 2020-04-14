fc79c2b38751d80a61a024e1745237a3edde9af6
Maxime Landon <maximelandon@gmail.com>
2020-04-14 17:09:42 +0200

    Add ghost functionalitycode for netstat

---
+++ ghost/netstat/netstat.go
@@ -0,0 +1,90 @@
+// Wiregost - Golang Exploitation Framework
+// Copyright © 2020 Para
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package netstat
+
+import (
+	"fmt"
+	"net"
+)
+
+// SockAddr represents an ip:port pair
+type SockAddr struct {
+	IP   net.IP
+	Port uint16
+}
+
+func (s *SockAddr) String() string {
+	return fmt.Sprintf("%v:%d", s.IP, s.Port)
+}
+
+// SockTabEntry type represents each line of the /proc/net/[tcp|udp]
+type SockTabEntry struct {
+	ino        string
+	LocalAddr  *SockAddr
+	RemoteAddr *SockAddr
+	State      SkState
+	UID        uint32
+	Process    *Process
+}
+
+// Process holds the PID and process name to which each socket belongs
+type Process struct {
+	Pid  int
+	Name string
+}
+
+func (p *Process) String() string {
+	return fmt.Sprintf("%d/%s", p.Pid, p.Name)
+}
+
+// SkState type represents socket connection state
+type SkState uint8
+
+func (s SkState) String() string {
+	return skStates[s]
+}
+
+// AcceptFn is used to filter socket entries. The value returned indicates
+// whether the element is to be appended to the socket list.
+type AcceptFn func(*SockTabEntry) bool
+
+// NoopFilter - a test function returning true for all elements
+func NoopFilter(*SockTabEntry) bool { return true }
+
+// TCPSocks returns a slice of active TCP sockets containing only those
+// elements that satisfy the accept function
+func TCPSocks(accept AcceptFn) ([]SockTabEntry, error) {
+	return osTCPSocks(accept)
+}
+
+// TCP6Socks returns a slice of active TCP IPv4 sockets containing only those
+// elements that satisfy the accept function
+func TCP6Socks(accept AcceptFn) ([]SockTabEntry, error) {
+	return osTCP6Socks(accept)
+}
+
+// UDPSocks returns a slice of active UDP sockets containing only those
+// elements that satisfy the accept function
+func UDPSocks(accept AcceptFn) ([]SockTabEntry, error) {
+	return osUDPSocks(accept)
+}
+
+// UDP6Socks returns a slice of active UDP IPv6 sockets containing only those
+// elements that satisfy the accept function
+func UDP6Socks(accept AcceptFn) ([]SockTabEntry, error) {
+	return osUDP6Socks(accept)
+}
