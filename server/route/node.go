c4864c08d875697e01e4d19c863736b3fa061396
Maxime Landon <maximelandon@gmail.com>
2020-06-29 16:47:44 +0200

    Add code to routing

--- server/route/node.go
+++ server/route/node.go
@@ -1,5 +1,24 @@
 package route
 
+import "net/url"
+
 // Node - A proxy node, mainly used to construct a proxy chain
 type Node struct {
+	ID        uint32
+	GhostID   uint32
+	Addr      string
+	Host      string
+	Protocol  string
+	Transport string
+	Remote    string   // Remote address, used by tcp/udp port forwarding
+	url       *url.URL // Raw url
+	User      *url.Userinfo
+	Values    url.Values
+
+	// DialOptions      []DialOption
+	// HandshakeOptions []HandshakeOption
+	// ConnectOptions   []ConnectOption
+	// Client           *Client
+	// marker           *failMarker
+	// Bypass           *Bypass
 }
--- server/route/route.go
+++ server/route/route.go
@@ -1,5 +1,10 @@
 package route
 
 // Route - Should be probably be deleted, because we might not need it
+// Route is somewhat different from a Chain object: the Route object does
+// NOT keep state of any transport mechanism, which means that the Route is also the object
+// that is stored in the database, displayed by users. They can also modify them, clone them,
+// and use them as models. Then, the Route is parsed into a Chain object, which will actually
+// implement the routed/proxied/forwarded communication.
 type Route struct {
 }
