e9fe6f35d01069ca08915ff1eaac8ed5909b08b9
Maxime Landon <maximelandon@gmail.com>
2020-08-05 13:32:08 +0200

    Delete old module structure

--- server/modules/base/ideas.go
+++
@@ -1,7 +0,0 @@
-package base
-
-// AskUserConfirm - Some actions performed by the module might require user permission,
-// because of context awareness needs that a computer is obviously unable to have.
-func (m *Module) AskUserConfirm() (ok bool) {
-	return
-}
