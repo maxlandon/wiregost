82e1c02dcbe80774f8030b08e05de393e0af7faa
Maxime Landon <maximelandon@gmail.com>
2020-08-07 05:57:25 +0200

    Add code + comments to Payload and PayloadDriver

--- server/module/payload.go
+++ server/module/payload.go
@@ -1,15 +1,66 @@
 package module
 
+import (
+	ghostpb "github.com/maxlandon/wiregost/proto/v1/gen/go/ghost"
+	modulepb "github.com/maxlandon/wiregost/proto/v1/gen/go/module"
+)
+
 // Payload - A pseudo-module dedicated to payload generation and staging.
+// It is not meant to be used and loaded on the Module Stack binary, because it would mean
+// exposing the whole handler system with an RPC. Although, these modules will lie in the Module tree.
+//
+// A Payload module has no Run(cmd string, args []string) method, because it is not exploiting
+// Instead, it sets up, through various functions, listeners and compilation details,
+// before executing a command such as generate, to_handler, etc...
 type Payload struct {
-	// Includes a ghost implant profile (format, arch, compilation, transport-agnostic)
-	// A ghost implant as bytes, for stagers
-	// Some transports
+	// Base module. Allows us to us the module's logging system. The exploit driver
+	// will decide, based on user choices, if the local debugging (happening in
+	// most of these functions below) should be pushed back to the user console.
+	*module
+
+	// Ghost implant core (transport-agnostic) profile (format, arch, etc.)
+	Info *ghostpb.Ghost
+
+	Stage       []byte         // A ghost implant as bytes, for stagers
+	StageConfig *ghostpb.Ghost // The configuration of the stage, for compabitibility checks
+	IsCompiled  bool           // If this is true, that means a ghost implant is already ready as Bytes
+
 	// Some security (limits, permissions, certificates, etc)
-	// Some working hours
+
+	// Some transport strings, passed at some point by a PayloadDriver
+	Transports []string
+
+	// Some working hours (Same, probably passed by a PayloadDriver, used by transports)
+	// We might have an option to override payload working hours with transport ones, or the inverse
+
 	// Some OS specifics
 }
 
-// Some of these functions will be hidden and called internally only
-
 // Generate (with optional saving of the implant as file, or as bytes).
+
+func (m *Payload) Init(meta *modulepb.Info) (err error) {
+
+	// Parses the protobuf metadata (base module function)
+	err = m.information(meta)
+
+	// Checks various fields and adds some if needed. (Type-specific)
+	meta.Type = modulepb.Type_PAYLOAD // Set module type
+
+	// Setup logger here. It should be somehow hooked to the stack binary
+	// gRPC connection with the Module Manager, with the ModuleLog stream.
+
+	// The logger cannot be initiated from the module.Module base type,
+	// because the method is unexported, and therefore cannot be called
+	// from the stack binary.
+	// Depending on which binary is calling this function we might pass
+	// it a different logger (on the server a local one, on stack a remote)
+	var isRemote bool
+
+	if isRemote {
+		// This instance is on the stack binary: start logger wired to gRPC.
+	} else {
+		// This instance is on the server: start a local logger.
+	}
+
+	return nil
+}
--- server/module/payload_driver.go
+++ server/module/payload_driver.go
@@ -1,19 +1,35 @@
 package module
 
 // PayloadDriver - A driver for payload construction, setup, compilation and staging.
+// An instance is created each time a user loads up a payload module.
+// Drivers are, in some sort, templates that also enhance the way information is accessible to console
+// users, as well as the way they can modify it, maintaining granularity as well as good context scoping.
+//
 // It plays a role similar to ExploitDriver, thereby offering multiple possibilities
 // for configuring per OS, per transport, and for potentially adding security modules.
-// These modules would enhance the set of granularity/functionality/automation we'd like.
 //
-// It is somehow a bit different from an ExploitDriver:
-// - A Payload module has no Run(cmd string, args []string) method, because it is not exploiting
-//   Instead, it sets up, through various functions, listeners and compilation details,
-//   before executing a command such as generate, to_handler, etc...
-//
-// A PayloadDriver instance is created each time a user loads up a payload module.
+// It is somehow a bit different from an ExploitDriver though:
+// - While the ExploitDriver can only have one Exploit module, the PayloadDriver can have one Payload,
 type PayloadDriver struct {
-	// Manages some handlers (they pull their configuration out of the implant's transports)
 	// Manages a Payload module
+	Payload *Payload
+
+	// Transports. They are handled and stored by the driver because they will be used by it
+	// in different cases: for compiling implants with these transports information, as well
+	// starting handlers based on them, which will need the routing system to work correctly.
+	// Might need some route chains as well.
+	Transports []*Transport
+
+	// Manages some handlers (they pull their configuration out of the implant's transports)
+
+	// The payload driver might have to access Wiregost's routing system. Maybe not.
+
+	ReadyForExploit bool // Used to signal at least one payload or Transport is ready,
+	// so a remote exploit can run and will find a listener/binder back soon.
 }
 
-// Instantiation methods.
+// NewPayloadDriver - Instantiate the driver with some core information and components.
+func NewPayloadDriver() (p *PayloadDriver) {
+	p = &PayloadDriver{}
+	return
+}
