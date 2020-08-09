092e733dcd7dddf4ec3e9839d174388d3409ef82
Maxime Landon <maximelandon@gmail.com>
2020-08-10 00:07:35 +0200

    Add new Module base, derived by all components needing UI access in Wiregost

--- server/module/module.go
+++ server/module/module.go
@@ -3,51 +3,62 @@ package module
 import (
 	"fmt"
 
+	dbpb "github.com/maxlandon/wiregost/proto/v1/gen/go/db"
 	modulepb "github.com/maxlandon/wiregost/proto/v1/gen/go/module"
 	"github.com/sirupsen/logrus"
 )
 
-// module - The base object inherited by all module types in Wiregost. This object is, at its core,
-// a client object that synchronises state with an equivalent Stack module, and can perform some
-// base operations on it: getting information, setting options, running checks, running it.
-// Some of this object's methods will make use of gRPC client calls, to sync with their Stack peer.
+// Module - The base object inherited by all Module types in Wiregost. Any component in Wiregost that
+// needs to be exposed to a User Interface (Console or RPC) can expose itself by embedding this type.
 //
-// ----- Exposure of the module type  ------
-// This type is unexported, and only some of its methods are. This has several reasons:
-// - This object will be instantiated in two places: on stack and on server.
-//   Thus some functions will be useless if called in the wrong binary.
-// - Many functions are only called by drivers and module subtypes in the same package.
-// - The user needs to access some base behavior and state, so these functions
-//   are exported for module authors..
-type module struct {
-	info *modulepb.Info // Info (protobuf object)
-
-	// We have commands, separated from info and options
-	commands map[string]*modulepb.Command
-
-	// We don't need options, because they are used only by the Stack module peer.
-	Opts map[string]*Option
+// As well, because the Module must provide scoped behavior when being run in a stack binary (which
+// runs the author's module Run() func). This object therefore has methods that can be used by the
+// binary stack for handling it correctly.
+// Some of this object' methods will make use of gRPC client calls, to sync with their Stack peer.
+type Module struct {
+	Info     *modulepb.Info               // Base Information
+	Commands map[string]*modulepb.Command // Module subcommands, can be added by embedders
+	Opts     map[string]Option            // Module options, with utility methods on them
 
 	// Logging: the module pushes messages to consoles through its logger
+	// This should prove useful to identify precisely the source of events/
+	// messages, because this logger will be derived by container types.
+	// Ex (in Exploit subtype):
+	// e.Log = e.Log.WithFields("exploit")
 	Log *logrus.Entry
+
+	// Owner - The user of this module instance. This is important as it will enable
+	// us to further check permissions at various stages of a module execution.
+	Owner *dbpb.User
 
 	// We need methods for interacting either with :
 	// the stack module equivalent when this module is in the server
 	// the server module equivalent when this module is on the stack
-	// Can be done at the level up, by the manager handling these modules
+	HasStackPeer bool // Does this module has an equivalent instance on a stack binary ?
+}
+
+// New - Instantiates a new module object, and populate it with the base information provided.
+//
+// This function is called twice: on server and on stack binary. The server should call it
+// for the according entry in the result of ToProtobuf(), called by the stack binary.
+func New(meta *modulepb.Info) (m *Module) {
+	return &Module{Info: meta}
 }
 
 // information - Registers the module's metadata information
-func (m *module) information(meta *modulepb.Info) (err error) {
-	m.info = meta
+func (m *Module) information(meta *modulepb.Info) (err error) {
+	m.Info = meta
 	return
 }
 
 // AddCommand - Adds a command to the module, which is used by the user like 'run check'.
-// - name           - The name of the command
-// - description    - A description for this command
-// - hasPayload     - True if this command triggers a remote exploit, requiring a payload.
-func (m *module) AddCommand(name, description string, hasPayload bool) {
+// @ name           - The name of the command
+// @ description    - A description for this command
+// @ hasPayload     - True if this command triggers a remote exploit, requiring a payload.
+//
+// This function is called twice: on server and on stack binary. The server should call it
+// for every entry in the result of ToProtobuf(), called by the stack binary.
+func (m *Module) AddCommand(name, description string, hasPayload bool) {
 	// Skip command if name is empty. Do not raise errors
 
 	// Trim spaces, don't want bad surprises because of typos.
@@ -56,19 +67,19 @@ func (m *module) AddCommand(name, description string, hasPayload bool) {
 // ToProtobuf - Returns the module with its information, options and commands. Used to share
 // state back and forth between consoles, the server and stacks. All categories are separately
 // parsed, because this amazing module structure dictates (at least) that.
-func (m *module) ToProtobuf() (mod *modulepb.Module) {
+func (m *Module) ToProtobuf() (mod *modulepb.Module) {
 
 	// Base information
-	mod = &modulepb.Module{Info: m.info}
+	mod = &modulepb.Module{Info: m.Info}
 
 	// Commands
-	for name, cmd := range m.commands {
+	for name, cmd := range m.Commands {
 		mod.Commands[name] = cmd
 	}
 
 	// Options
 	for name, opt := range m.Opts {
-		mod.Options[name] = opt.info
+		mod.Options[name] = opt.toProtobuf()
 	}
 
 	return
@@ -76,26 +87,36 @@ func (m *module) ToProtobuf() (mod *modulepb.Module) {
 
 // SetupLog - An exported function that will be called by the Stack module object. This logger will have
 // hooks linked to a gRPC service, for sending back status to user consoles and logging files.
-func (m *module) SetupLog() (err error) {
+func (m *Module) SetupLog() (logger *logrus.Entry) {
+
+	// When this function is called, we should have determined if the module has a remote side:
+	// If yes, we add a special hook for logging over gRPC, or we make a completely different
+	// logrus.Logger in a specialized function setupLogRemote() (logger *logrus.Logger)
+	// to which we then add fields.
+
+	// Add fields for userID or ClientID
+
+	// Add WithFields("module") if needed
+
 	return
 }
 
 // Asset - Find the path of an asset in the module source directory.
 // It is exported, because authors will need to access/use non-Go files.
-func (m *module) Asset(path string) (filePath string, err error) {
+func (m *Module) Asset(path string) (filePath string, err error) {
 	return
 }
 
 // AskUserConfirm - Some actions performed by the module might require user permission,
 // because it has some context awareness needs that a computer is obviously unable to have.
-func (m *module) AskUserConfirm() (ok bool) {
+func (m *Module) AskUserConfirm() (ok bool) {
 	return
 }
 
 // PreRunChecks - All checks for commands, options, etc. are done in this function.
 // Does not concern various compatibility and perms checks done by drivers, it is
 // merely utility checking code to avoid useless troubles.
-func (m *module) PreRunChecks(cmd string) (err error) {
+func (m *Module) PreRunChecks(cmd string) (err error) {
 
 	err = m.CheckCommand(cmd)
 	if err != nil {
@@ -113,19 +134,19 @@ func (m *module) PreRunChecks(cmd string) (err error) {
 // CheckRequiredOptions - Checks that all required options have a value. The advantage of this
 // function is that it can check for options that have been registered by module subtypes as
 // well, as they all lie in the same Opts object.
-func (m *module) CheckRequiredOptions() (err error) {
+func (m *Module) CheckRequiredOptions() (err error) {
 	return
 }
 
 // CheckCommand - Verifies the command run by the user (like 'run exploit' or 'run check') is valid.
-func (m *module) CheckCommand(command string) (err error) {
+func (m *Module) CheckCommand(command string) (err error) {
 	// If we don't have commands it means there is no need for them, so no error
-	if len(m.commands) == 0 || m.commands == nil {
+	if len(m.Commands) == 0 || m.Commands == nil {
 		return nil
 	}
 
 	// Else check for it
-	for _, cmd := range m.commands {
+	for _, cmd := range m.Commands {
 		if cmd.Name == command {
 			return nil
 		}
@@ -136,6 +157,13 @@ func (m *module) CheckCommand(command string) (err error) {
 // Run method (called by driver) using the provided module subcommand (run 'check_vuln')o
 // This will trigger the appropriate function in the Stack module itself, while preserving
 // the ability to use drivers such as ExploitDriver for managing the full process of an exploit.
-func (m *module) run() (err error) {
+func (m *Module) run() (err error) {
+	return
+}
+
+// Cleanup - Clean any state needed for this module. This function is here more to remind
+// all types embedding this module that they may override it, as a good practice of cleaning.
+func (m *Module) Cleanup() (err error) {
+
 	return
 }
--- server/module/option.go
+++ server/module/option.go
@@ -8,17 +8,66 @@ import (
 	"regexp"
 	"strconv"
 
-	modulepb "github.com/maxlandon/wiregost/proto/v1/gen/go/module"
+	pb "github.com/maxlandon/wiregost/proto/v1/gen/go/module"
 )
 
-// Option - A module option, with many methods for processing its content and
-// returning it in an optimal format, such as a range of net.IPs if we want it.
-// Note that all option values are strings: you can process/transform them as
-// you wish with Go's std/community libraries.
-// However, the Option type provides some utility methods for processing its content
-// and returning it in an optimal format, such as an int, a file object, a range of net.IPs, etc...
-type Option struct {
-	info *modulepb.Option // This is the base information.
+// Option - A module option, with methods for processing its content and returning it in an optimal format,
+// such as a range of net.IPs if we want it. Note that all option values are strings: you can process
+// or transform them as you wish with Go's std/community libraries.
+// However, the option type provides some utility methods for processing its content and
+// returning it in an optimal format, such as an int, a file object, a range of net.IPs, etc.
+type Option interface {
+	Name() string
+	Category() string
+	Value() string
+	Description() string
+	Required() bool
+	Set(string) error
+	// toProtobuf - Unexported, so that No types in any other
+	// package can implement the Option interface. Included because
+	// it is always at the module.Module level that we pack protobuf
+	// information for sending to sharing with other components.
+	toProtobuf() *pb.Option
+}
+
+// option - The concrete type if unexported because we do not want
+// any module to have such control on what an option may do.
+type option struct {
+	i *pb.Option // This is the base information.
+}
+
+// Name - Implements the Option interface
+func (o *option) Name() string {
+	return o.i.Name
+}
+
+// Category - Implements the Option interface
+func (o *option) Category() string {
+	return o.i.Category
+}
+
+// Value - Implements the Option interface
+func (o *option) Value() string {
+	return o.i.Value
+}
+
+// Description - Implements the Option interface
+func (o *option) Description() string {
+	return o.i.Description
+}
+
+// Required - Implements the Option interface
+func (o *option) Required() bool {
+	return o.i.Required
+}
+
+// Required - Implements the Option interface
+func (o *option) Set(value string) (err error) {
+	if value == "" && o.i.Required {
+		return fmt.Errorf("Cannot set %s to empty: option is required.", o.i.Name)
+	}
+	o.i.Value = value
+	return
 }
 
 // AddOption - Instantiates an option for this module, populating it with given parameters.
@@ -27,7 +76,10 @@ type Option struct {
 // value          - If non-empty, this will serve a default value. This field is used for the option value.
 // description    - Option description
 // required       - Is this option required ? If true, the Value field cannot be empty.
-func (m *module) AddOption(name, category, value, description string, required bool) (err error) {
+//
+// This function is called twice: on server and on stack binary. The server should call it
+// for every entry in the result of ToProtobuf(), called by the stack binary.
+func (m *Module) AddOption(name, category, value, description string, required bool) (err error) {
 
 	// Invalid cases: Name is nil, and option is required but has not been provided a default value
 	if name == "" || (value == "" && required) {
@@ -35,8 +87,8 @@ func (m *module) AddOption(name, category, value, description string, required b
 	}
 
 	// Populate
-	opt := &Option{
-		&modulepb.Option{
+	opt := &option{
+		&pb.Option{
 			Name:        name,
 			Category:    category,
 			Value:       value,
@@ -53,7 +105,7 @@ func (m *module) AddOption(name, category, value, description string, required b
 
 // Option - Returns one of the module's options, by name, with an error. This function is a safer but less handy
 // alternative to the Options["Name"] way of querying options, because you can check immediately for key presence
-func (m *module) Option(name string) (opt *Option, err error) {
+func (m *Module) Option(name string) (opt Option, err error) {
 	if opt, found := m.Opts[name]; found {
 		return opt, nil
 	}
@@ -61,23 +113,35 @@ func (m *module) Option(name string) (opt *Option, err error) {
 	return nil, fmt.Errorf("module asked for an unknown option: %s", name)
 }
 
+func (o *option) toProtobuf() (opt *pb.Option) {
+
+	return &pb.Option{
+		Name:        o.i.Name,
+		Category:    o.i.Category,
+		Value:       o.i.Value,
+		Description: o.i.Description,
+		Required:    o.i.Required,
+	}
+
+}
+
 // String - Implements the string interface, and returns the option value as a string.
-func (o *Option) String() string {
-	return o.info.Value
+func (o *option) String() string {
+	return o.i.Value
 }
 
 // Bool - Returns the option value as a boolean, with an optional error, just in case
-func (o *Option) Bool() (bool, error) {
+func (o *option) Bool() (bool, error) {
 	// Allow for both expressions, and it must be required because its bool
-	if (o.info.Value == "true" || o.info.Value == "yes") && o.info.Required {
+	if (o.i.Value == "true" || o.i.Value == "yes") && o.i.Required {
 		return true, nil
 	}
-	return false, fmt.Errorf("option %s is not a boolean, or uncorrectly set (should not happen)", o.info.Name)
+	return false, fmt.Errorf("option %s is not a boolean, or uncorrectly set (should not happen)", o.Name)
 }
 
 // Int - Returns the option value as an int
-func (o *Option) Int() (int, error) {
-	port, err := strconv.Atoi(o.info.Value)
+func (o *option) Int() (int, error) {
+	port, err := strconv.Atoi(o.i.Value)
 	if err != nil {
 		return -1, err
 	}
@@ -85,30 +149,30 @@ func (o *Option) Int() (int, error) {
 }
 
 // Regexp - Returns a Regexp object, enabling to deal with various regular expression problems
-func (o *Option) Regexp() (reg *regexp.Regexp, err error) {
-	reg, err = regexp.Compile(o.info.Value)
+func (o *option) Regexp() (reg *regexp.Regexp, err error) {
+	reg, err = regexp.Compile(o.i.Value)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse option %s as regexp: %s", o.info.Name, err)
+		return nil, fmt.Errorf("failed to parse option %s as regexp: %s", o.Name, err)
 	}
 	return
 }
 
 // File - Returns a file object. We can then get/set its properties and read/write it.
-func (o *Option) File() (file *os.File) {
+func (o *option) File() (file *os.File) {
 	return
 }
 
 // IP - Parses a string into a net.IP object, supporting both IPv4 and IPv6. This function is not redundant with option.ToAddressRange():
 // both provide different objects for different uses, and we assume the module author will know which of these parsing function to use.
-func (o *Option) IP() (net.IP, error) {
-	ip := net.ParseIP(o.info.Value)
+func (o *option) IP() (net.IP, error) {
+	ip := net.ParseIP(o.i.Value)
 	if ip.String() == "<nil>" {
-		return nil, fmt.Errorf("failed to parse IP address for option %s ", o.info.Name)
+		return nil, fmt.Errorf("failed to parse IP address for option %s ", o.Name)
 	}
 	return ip, nil
 }
 
 // ToAddressRange - Parses an option
-func (o *Option) ToAddressRange() {
+func (o *option) ToAddressRange() {
 
 }
